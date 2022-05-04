0312bd146d1db3fec2e600b5002a8ce743052ac0
nico4108 <nicolaj.spiegel@gmail.com>
2022-05-04 12:30:44 +0200

    kafka

--- Mandatory/kafka.py
+++ Mandatory/kafka.py
@@ -2,6 +2,7 @@ from bottle import get, run, response, post, request
 import json, yaml
 from dict2xml import dict2xml
 import pandas as pd
+import sqlite3
 
 users = {
     '12345':{'id':'1', 'email':'@a', 'token':'12345'},
@@ -9,34 +10,44 @@ users = {
 }
 
 messages = {
-    '1':[
+    'patient':[
         {
-            'id':'aaa0381a-4a37-4caf-af5f-aae31933749c', 
-            'm1':'Hi', 
+            'id':'aaa0381a-4a37-4caf-af5f-aae31933749c',
+            'name': 'Karen Hansen', 
+            'cpr':'112233-4454',
+            'adress': 'Vejen 5, Vejen 6666',
             'access':'*'
         },
         {
-            'id':'6a66e5e7-70c9-4957-83ad-83e75e4daba0', 
-            'm2':'Hi', 
-            'access':'*'
-        },
-        {
-            'id':'5564f18d-0bed-47db-b5cf-1482c7330544', 
-            'm3':'Hi', 
-            'access':'*'
-        },
-        {
-            'id':'fd8d94db-e733-42df-b87d-e77b1c43a023', 
-            'm4':'Hi', 
+            'id':'aaa0381a-4a37-4caf-af5f-hjkjkksksks',
+            'name': 'Leif Badingado', 
+            'cpr':'556644-1091',
+            'adress': 'Roaden 10, Road 8888',
             'access':'*'
         }
     ]  
 }
+recept = {
+                'recept': [
+                {
+                    'doc': 'Donald Doc',
+                    'medicine': 'Morphine',
+                    'amount': '200mg'
+                }
+            ]
+}
+
+def row_to_dict(cursor: sqlite3.Cursor, row: sqlite3.Row) -> dict:
+            data = {}
+            for idx, col in enumerate(cursor.description):
+                data[col[0]] = row[idx]
+            return data
 
 ############################
-@get('/provider/<id>/token/<token>/format/<format>')
-def _(id, token, format):
+@get('/provider/<prov_id>/cpr/<cpr>/token/<token>/format/<format>')
+def _(prov_id, token, format, cpr):
     try:
+        conn = sqlite3.connect('Mandatory.db')
         '''if limit == 0:
             raise Exception('Limit cannot be 0')'''
         if token not in users:
@@ -44,12 +55,16 @@ def _(id, token, format):
         
         if format == 'JSON':
             response.content_type = 'application/json'
-            return json.dumps(messages[id])
+            c = conn.cursor()
+            c.row_factory = row_to_dict
+            c.execute(f"SELECT * FROM patient WHERE cpr='{cpr}'")
+            result = c.fetchall()
+            return json.dumps(result)
 
         elif format == 'XML':
             response.content_type = 'application/xml'
             xml_list = []
-            for msg in messages[id]:
+            for msg in messages[prov_id]:
                 xml_list.append(msg)
 
             data = '<?xml version="1.0" encoding="UTF-8"?>'
@@ -61,27 +76,36 @@ def _(id, token, format):
         elif format == 'YAML':
             response.content_type = 'application/yaml'
             data = 'messages:' + '\n'
-            data += yaml.dump(messages[id])
+            data += yaml.dump(messages[prov_id])
             return data
 
         elif format == 'TSV':
-            data = pd.DataFrame.from_records(messages[id])
+            data = pd.DataFrame.from_records(messages[prov_id])
             return data.to_csv(sep='\t', index=False)
+
 
     except Exception as ex:
         response.status = 400
         return str(ex)
 
+#TODO: Split request ud så ikke IF ELSE men egne requests!! <-----
+#TODO: Post/GET recept og patien data til databasen for alle filtyper!
+
 @post('/provider/<pro_id>/token/<token>/format/<format>')
 def _(pro_id, token, format):
     # provider skal være subject(student, teacher....)
     try: 
+        conn = sqlite3.connect('Mandatory.db')
+        #conn.execute("CREATE TABLE recept (doc char(50), medicine char(50), amount char(8), recept_cpr char(11), FOREIGN KEY(recept_cpr) REFERENCES patient(cpr))")
+
         if token not in users:
             raise Exception('Token is invalid')
 
         if format == 'JSON':
             response.content_type = 'application/json'
             message = request.json
+            #conn.execute(f"INSERT INTO patient (id, name, cpr, adress, access) VALUES ('{message['id']}', '{message['name']}', '{message['cpr']}', '{message['adress']}', '{message['access']}')")
+            #conn.commit()
             print(message)
 
             return message
